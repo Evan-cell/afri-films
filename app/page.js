a64dfb7bc56bc8b5ee98d583e9a8ebf3425afbfd
evan <malcomchege0582@gmail.com>
2023-11-13 15:37:06 +0300

    afri nav

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Header from '@/components/Header'
+import Nav from '@/components/Nav'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="">
      <Header />
+     <Nav />
     </main>
   )
 }
